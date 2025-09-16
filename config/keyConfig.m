84e31a59cf4853472256b53fae03802dd4c8da7f
Uriel LASCOMBES uriel.lascombes@univ-amu.fr <ulascombes@login02.cluster>
2025-09-16 08:57:50 +0200

    corrections on keys

--- config/keyConfig.m
+++ config/keyConfig.m
@@ -21,15 +21,11 @@ KbName('UnifyKeyNames');
 if const.scanner == 0
     my_key.mri_trVal        =   's';                    % mri trigger letter
     my_key.left1Val         =   'r';                    % left button 1
-    my_key.left2Val         =   'q';                    % left button 1
     my_key.right1Val        =   'u';                    % right button 1
-    my_key.right2Val        =   'm';                    % right button 1
 elseif const.scanner == 1
     my_key.mri_trVal        =   't';                    % mri trigger letter
     my_key.left1Val         =   'r';                    % left button thumb (outside)r
-    my_key.left2Val         =   'g';                    % left button index (inside)g
     my_key.right1Val        =   'b';                    % right button thumb (outside)b
-    my_key.right2Val        =   'y';                    % right button index (inside)y
 end
 
 my_key.escapeVal        =   'escape';               % escape button
